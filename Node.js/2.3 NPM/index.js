0644d057528f1f704100f2404cebf333da9bce7e
Lily Eckhart <64758489+lilyeckhart@users.noreply.github.com>
2025-01-21 10:34:48 -0600

    Uploaded NPM modules

---
+++ Node.js/2.3 NPM/index.js
@@ -0,0 +1,8 @@
+// import generateName from "sillyname";
+import superheros from "superheros";
+
+// var sillyName = generateName();
+// console.log(`My name is ${sillyName}.`);
+
+const name = superheros.random();
+console.log(`I am ${name}!`);
